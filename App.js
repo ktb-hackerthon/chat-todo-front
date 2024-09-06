a5df7b9e4fd3698bb5f6f0bb0e4b41b986bfd0fa
PARK-SSOYEON <soyeon00307@gmail.com>
2024-09-06 10:33:14 +0900

    feat: 유효Id 생성 및 확인 추후 alert 지우기

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components/native';
 import Header from './src/components/Header';
 import Calendar from './src/pages/Calendar';
 import Chatting from './src/pages/Chatting';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
     const [currentScreen, setCurrentScreen] = useState('Chatting');
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+        const checkUserId = async () => {
+            try {
+                const storedUserId = await AsyncStorage.getItem('userId');
+                if (!storedUserId) {
+                    const newUserId = uuidv4({
+                        random: Array.from({ length: 16 }, () => Math.floor(Math.random() * 256))
+                    });
+                    await AsyncStorage.setItem('userId', newUserId);
+                    setUserId(newUserId);
+                    alert("새 아이디: "+ newUserId);
+                } else {
+                    setUserId(storedUserId);
+                    alert("저장된 아이디: "+ storedUserId);
+                }
+            } catch (error) {
+                alert("에러");
+            }
+        };
+
+        checkUserId();
+    }, []);
 
     const handleNavigate = () => {
         setCurrentScreen((prevScreen) => (prevScreen === 'Calendar' ? 'Chatting' : 'Calendar'));
     }
+
     return (
       <SafeAreaView>
         <Container>
